55463fc27422f3b010a47a3ba10d489952ea0af1
isebarn <isebarn182@gmail.com>
2020-08-21 16:00:17 +0000

    Robots

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -18,7 +18,7 @@ LOG_FORMAT = '%(levelname)s: %(message)s'
 #USER_AGENT = 'tutorial (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
